ce27a865fa0b47cd0ac1db21cde7f9bda8b4d8cb
Tarun Alapati <f20190733@hyderabad.bits-pilani.ac.in>
2022-11-19 17:38:04 +0530

    Created Home Page

--- app/src/main/java/com/example/mindspace/MainActivity.java
+++ app/src/main/java/com/example/mindspace/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.mindspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
